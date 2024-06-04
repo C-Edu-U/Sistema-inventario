8635e765d102bc79d07a74ac2821cbf38aa02065
C-Edu-U <eduardo3444@gmail.com>
2024-06-04 18:01:05 -0400

    Se Agregó columnas al sistema

--- inventory_system/inventory/admin.py
+++ inventory_system/inventory/admin.py
@@ -5,11 +5,41 @@ class DetalleVentaInline(admin.TabularInline):
     model = DetalleVenta
     extra = 1
 
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion')
+    search_fields = ('nombre',)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion', 'precio_unitario', 'existencia', 'categoria')
+    list_filter = ('categoria',)
+    search_fields = ('nombre',)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'apellidos', 'email', 'telefono')
+    search_fields = ('nombre', 'apellidos', 'email')
+
+@admin.register(Venta)
 class VentaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cliente', 'vendedor', 'fecha', 'total')
+    list_filter = ('fecha', 'vendedor')
+    search_fields = ('cliente__nombre', 'cliente__apellidos', 'vendedor__username')
+    date_hierarchy = 'fecha'
     inlines = [DetalleVentaInline]
 
-admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(Cliente)
-admin.site.register(Venta, VentaAdmin)
-admin.site.register(DetalleVenta)
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.prefetch_related('cliente', 'vendedor')
+        return queryset
+
+@admin.register(DetalleVenta)
+class DetalleVentaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'venta', 'producto', 'cantidad', 'precio')
+    search_fields = ('venta__cliente__nombre', 'producto__nombre')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.prefetch_related('venta', 'producto')
+        return queryset
